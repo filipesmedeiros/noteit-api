18624c6f37990de3cbda82f52be7c8b42239e39f
Filipe Medeiros <filipesilvamedeiros@gmail.com>
2019-03-28 11:46:01 +0000

    Try new deploy config (on eu-west-2 London)

--- services/notes/list.js
+++ services/notes/list.js
@@ -1,5 +1,5 @@
-import * as dynamoDbLib from "../libs/dynamodb-lib";
-import { success, failure } from "../libs/response-lib";
+import * as dynamoDbLib from "../../libs/dynamodb-lib";
+import { success, failure } from "../../libs/response-lib";
 
 export async function main(event, context) {
     // process.env is accessing the environment variables at runtime, so the IDE throws a warning
