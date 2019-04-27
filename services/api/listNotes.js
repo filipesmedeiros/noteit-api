51c9a866100d20677880117cb8f7a831200ca385
Filipe Medeiros <filipesilvamedeiros@gmail.com>
2019-04-27 22:33:04 +0100

    Debugging...

--- services/api/listNotes.js
+++ services/api/listNotes.js
@@ -2,6 +2,8 @@ import * as dynamoDbLib from './libs/dynamodb-lib';
 import { success, failure } from './libs/response-lib';
 
 export async function main(event, context) {
+    console.log(event);
+
     let params = {
         TableName: process.env.tableName,
         // 'KeyConditionExpression' defines the condition for the query
